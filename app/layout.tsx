a93bf728f6e37433f26aec029877f9336816b7fa
Baz Rahimi <bazrahimi@hotmail.com>
2024-06-19 22:18:38 +1000

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { inter } from '@/app/ui/font';
+import '@/app/ui/global.css';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
+
+// by adding Inter to the body, the fond will be applied throughout the application. 
