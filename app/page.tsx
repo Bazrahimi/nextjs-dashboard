65b168035a96a13b9d49377b75485f8cfb4f2a4b
Baz Rahimi <bazrahimi@hotmail.com>
2024-06-19 22:32:29 +1000

    add

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,7 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          {/* NOTE: image with dimensions and web fonts are common causes of layout shift due to the browser having to download additional resouces. */}
           <Image
             src="/hero-desktop.png"
             width={1000}
@@ -43,6 +44,7 @@ export default function Page() {
             src="/hero-mobile.png"
             width={560}
             height={620}
+            className='block md:hidden'
             alt="Screenshot of the dashboard project showiing desktop version"
           />
         </div>
