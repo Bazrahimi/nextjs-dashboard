a26207989338977cb5e6a0466f0f676598ed0364
Baz Rahimi <bazrahimi@hotmail.com>
2024-11-27 15:45:43 +1100

    add page pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,17 +4,33 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
+  const pathname = usePathname(); // get the current pathname of the url
+  const searchParams = useSearchParams(); // access the query parameters of the current url
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    // create a new instance of URLSearchParams from the current search parameters
+    const params = new URLSearchParams(searchParams);
+
+    // set or update the 'page' query parameters with the provided page number
+    params.set('pages', pageNumber.toString());
+
+    // construct the URL by combining the current pathname with updated query string.
+    return `${pathname}?${params.toString}`;
+  }
+
   // NOTE: Uncomment this code in Chapter 11
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -47,7 +63,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,4 +1,5 @@
 "use client";
+import {useDebouncedCallback} from 'use-debounce'
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
@@ -11,7 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const router = useRouter();
 
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
 
     if (term) {
@@ -21,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
