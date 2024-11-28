9a86b1481e743a10f5f56afc3866b822076bb177
Baz Rahimi <bazrahimi@hotmail.com>
2024-11-28 13:49:54 +1100

    add delete logic

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -54,3 +54,8 @@ export const updateInvoice = async(id: string, formData: FormData) => {
   redirect('/dashboard/invoices')
 
 }
+
+export const deleteInvoice = async (id:string) => {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices')
+}
